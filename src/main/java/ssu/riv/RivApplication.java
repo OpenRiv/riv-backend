e68992a22024259adae60f01cafef4a7a8cd9d9a
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-12-16 21:38:19 +0900

    feat: 임시로 자체인증서 사용

--- src/main/java/ssu/riv/RivApplication.java
+++ src/main/java/ssu/riv/RivApplication.java
@@ -24,8 +24,10 @@
 package ssu.riv;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
