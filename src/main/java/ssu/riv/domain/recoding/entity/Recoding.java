1a33ecd29afb03f7c8bc2b33cd27611857f6463c
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-12-18 07:27:35 +0900

    fix: recoding text 타입 변경

--- src/main/java/ssu/riv/domain/recoding/entity/Recoding.java
+++ src/main/java/ssu/riv/domain/recoding/entity/Recoding.java
@@ -15,6 +15,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
@@ -34,6 +35,7 @@ public class Recoding {
     @Column
     private String title;
     @Column
+    @Lob
     private String text;
 
     @ManyToOne(fetch = FetchType.LAZY)
