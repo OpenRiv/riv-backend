8ec12b4634b66cfbdcc6d25cc281ab0e3c8673af
김혜령 <hyeryoung27@soongsil.ac.kr>
2025-07-20 23:25:07 +0900

    fix: 의존성 수정

--- src/main/java/ssu/riv/RivApplication.java
+++ src/main/java/ssu/riv/RivApplication.java
@@ -24,21 +24,13 @@
 package ssu.riv;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.PostConstruct;
-import java.time.ZonedDateTime;
-import java.util.TimeZone;
-
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 @SpringBootApplication
 @EnableJpaAuditing
