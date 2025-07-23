02088fda3d0a629831c5ddf45dc8e25f9b6e3b38
김혜령 <hyeryoung27@soongsil.ac.kr>
2025-07-23 16:09:22 +0900

    Fix: 리다이렉트 수정

--- src/main/java/ssu/riv/domain/recoding/controller/RecodingController.java
+++ src/main/java/ssu/riv/domain/recoding/controller/RecodingController.java
@@ -1,15 +1,8 @@
 package ssu.riv.domain.recoding.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ssu.riv.domain.recoding.converter.RecodingConverter;
 import ssu.riv.domain.recoding.dto.RecodingRequest;
